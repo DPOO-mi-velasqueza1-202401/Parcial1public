f6becd1b79792920ffe9b355e9c20244aefaa29d
miva2003 <mi.velasqueza1@uniandes.edu.co>
2024-02-21 09:09:45 -0500

    intento 1

---
+++ src/consola/Consola.java
@@ -0,0 +1,30 @@
+package consola;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import logica.Hombre;
+
+public class Consola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String genero;
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			genero = read.readLine();
+			if (genero == "Hombre") {
+				double TMB;
+				TMB = logica.Hombre.
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
--- src/logica/TMB.java
+++ src/logica/TMB.java
@@ -2,11 +2,35 @@ package logica;
 
 public abstract class TMB {
 	
-	protected int pesoKg;
-	protected int alturaCm;
-	protected int edadAños;
+	private int pesoKg;
+	private int alturaCm;
+	private int edadAños;
 		
 	protected abstract double calcularTMB(int pesoKg, int alturaCm,  int edadAños);
+
+	public int getPesoKg() {
+		return pesoKg;
+	}
+
+	public void setPesoKg(int pesoKg) {
+		this.pesoKg = pesoKg;
+	}
+
+	public int getAlturaCm() {
+		return alturaCm;
+	}
+
+	public void setAlturaCm(int alturaCm) {
+		this.alturaCm = alturaCm;
+	}
+
+	public int getEdadAños() {
+		return edadAños;
+	}
+
+	public void setEdadAños(int edadAños) {
+		this.edadAños = edadAños;
+	}
 	
 		
 }
